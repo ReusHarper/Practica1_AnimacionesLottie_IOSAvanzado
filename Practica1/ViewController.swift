9d0f05669895efc9373176c04734d769ff7611fa
ReusHarper <reusharper@comunidad.unam.mx>
2022-11-10 21:29:24 -0600

    Initial Commit

---
+++ Practica1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica1
+//
+//  Created by ReusHarper on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
